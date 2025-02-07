a310561cfe3c72dac21c8e9aada5ee8f539af173
ViAarisKea <viaa0001@stud.kea.dk>
2025-02-07 13:55:40 +0100

    commit

---
+++ src/main/java/com/example/githubactions/GithubActionsStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsStartApplication.class, args);
+    }
+
+}
